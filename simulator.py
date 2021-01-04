ca9fbf3c139708e9a303b26f3f92f10f037548f1
Mr. Radium <ambroseutsav@gmail.com>
2021-01-04 17:18:12 +0530

    made the symbol search function

--- simulator.py
+++ simulator.py
@@ -4,10 +4,22 @@ import json
 # res = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=BAC&interval=5min&apikey=FLB281YD66I569JV')
 # print(res.text)
 
-def write_datafile(write_text):
-    datafile = open("datafile.json", "w")
-    datafile.write(write_text)
-    datafile.close()
+
+
+
+def print_data(times):
+
+    # Printing the Data in Formal Form
+    print('')
+    print("Symbol: " + data["bestMatches"][times]["1. symbol"])
+    print("Name: " + data["bestMatches"][times]["2. name"])
+    print("Type: " + data["bestMatches"][times]["3. type"])
+    print("Region: " + data["bestMatches"][times]["4. region"])
+    print("Market Open: " + data["bestMatches"][times]["5. marketOpen"])
+    print("Market Close: " + data["bestMatches"][times]["6. marketClose"])
+    print("TimeZone: " + data["bestMatches"][times]["7. timezone"])
+    print("Currency: " + data["bestMatches"][times]["8. currency"])
+    print("Match Score: "  + data["bestMatches"][times]["9. matchScore"])
 
 def sym_search(keywords):
     # Fetching Request Message
@@ -17,35 +29,28 @@ def sym_search(keywords):
     res = requests.get('https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=' + keywords + '&apikey=FLB281YD66I569JV')
 
     # Converting Data to JSON
+    global data
     data = json.loads(res.text)
 
     # Number of Results
+    global results
     results = len(data["bestMatches"])
 
-    # Writing Data to a JSON File (Not Mandatory)
-    # write_datafile(res.text)
-
-    # Printing the Data in Formal Form
     print("")
     print("There are "  + str(results) + " best macthes for yor request!")
-    print(data["bestMatches"])
-    # print(data["bestMatches"]["1. symbol"])
-    # print(data["bestMatches"]["2. name"])
-    # print(data["bestMatches"]["3. type"])
-    # print(data["bestMatches"]["4. region"])
-    # print(data["bestMatches"]["5. marketOpen"])
-    # print(data["bestMatches"]["6. marketClose"])
-    # print(data["bestMatches"]["7. timezone"])
-    # print(data["bestMatches"]["8. currency"])
-    # print(data["bestMatches"]["9. matchScore"])
+
+    i = 0
+    while i < results:
+        print_data(i)
+        i += 1
 
 
 task = input("What do you want to do: ")
 
 if "ls" in task:
-    try:
+#    try:
         keywords = input("Enter the Keywords: ")
         sym_search(keywords)
-    except:
-        print("Something might be wrong!")
-        print("Please check your internet connection and try again.")
+#    except:
+        # print("Something might be wrong!")
+        # print("Please check your internet connection and try again.")
