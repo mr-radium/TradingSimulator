29027dcb64d5930e58a95fa642091ba7d07e9d89
Mr. Radium <ambroseutsav@gmail.com>
2021-01-04 13:11:45 +0530

    made a virtual environment

--- simulator.py
+++ simulator.py
@@ -4,24 +4,46 @@ import json
 # res = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=BAC&interval=5min&apikey=FLB281YD66I569JV')
 # print(res.text)
 
+def write_datafile(write_text):
+    datafile = open("datafile.json", "w")
+    datafile.write(write_text)
+    datafile.close()
+
 def sym_search(keywords):
+    # Fetching Request Message
     print("Please wait while we are fetching your request...")
+
+    # Requesting Data
     res = requests.get('https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=' + keywords + '&apikey=FLB281YD66I569JV')
-    print(res.text)
+
+    # Converting Data to JSON
+    data = json.loads(res.text)
+
+    # Number of Results
+    results = len(data["bestMatches"])
+
+    # Writing Data to a JSON File (Not Mandatory)
+    # write_datafile(res.text)
+
+    # Printing the Data in Formal Form
+    print("")
+    print("There are "  + str(results) + " best macthes for yor request!")
+    print(data["bestMatches"]["1. symbol"])
+    print(data["bestMatches"]["2. name"])
+    print(data["bestMatches"]["3. type"])
+    print(data["bestMatches"]["4. region"])
+    print(data["bestMatches"]["5. marketOpen"])
+    print(data["bestMatches"]["6. marketClose"])
+    print(data["bestMatches"]["7. timezone"])
+    print(data["bestMatches"]["8. currency"])
+    print(data["bestMatches"]["9. matchScore"])
+
 
 task = input("What do you want to do: ")
 
 if "ls" in task:
     try:
-        keywords = input("Enter the Keywoards: ")
-        sym_search(keywords)
-    except:
-        print("Something might be wrong!")
-        print("Please check your internet connection and try again")
-
-if "ls -s" in task:
-    try:
-        keywords = task.split("ls -s") 
+        keywords = input("Enter the Keywords: ")
         sym_search(keywords)
     except:
         print("Something might be wrong!")
