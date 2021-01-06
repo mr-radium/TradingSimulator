cff70b02f98309cd86be0bd73dfe4c6e4f74589e
Mr. Radium <ambroseutsav@gmail.com>
2021-01-06 14:10:38 +0530

    added some more features

---
+++ get_current_stock_data.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+# URL (http://localhost:3000/nse/get_quote_info?companyName=ONGC)
+
+def get_current_stock_data_nse(stock):
+
+    print("Please wait while we are fetching your request.")
+
+    # If there's "&" symbol in the the stocky symbol it will replace it with "%26"
+    if "&" in stock:
+        stock = stock.replace("&", "%26")
+
+    # If there's "'" symbol in the the stocky symbol it will replace it with "%27"
+    if "'" in stock:
+        stock = stock.replace("'", "%27")
+
+    res = requests.get('http://localhost:3000/nse/get_quote_info?companyName=' + stock)
+    
+    stock_info = json.loads(res.text)
+    
+    if len(stock_info["data"]) == 0:
+        print("")
+        print("Sorry, we can't find the status of the following stock symbol.")
+
+    else:
+        print("")
+        print("Company Name: " + stock_info["data"][0]["companyName"])
+        print("Symbol: " + stock_info["data"][0]["symbol"])
+        print("")
+        print("Current Price: " + stock_info["data"][0]["lastPrice"])
+        print("Total Sell Quantity: " + stock_info["data"][0]["totalSellQuantity"])
+        print("Open Price: " + stock_info["data"][0]["open"] + " | Day High: " + stock_info["data"][0]["dayHigh"] + " | Day Low: " + stock_info["data"][0]["dayLow"])
+        print("")
+        print("Last Updated: " + stock_info["lastUpdateTime"])
--- simulator.py
+++ simulator.py
@@ -1,6 +1,8 @@
 # Importing the functions from the symbol_seacher file
+import os
 from symbol_searcher import sym_search
 from symbol_searcher import print_data
+from get_current_stock_data import get_current_stock_data_nse
 
 
 if __name__ == "__main__":
@@ -10,9 +12,27 @@ if __name__ == "__main__":
     if "ls" in task:
 
         try:
-                keywords = task.replace("ls ", "")
-                sym_search(keywords)
+            keywords = task.replace("ls ", "")
+            sym_search(keywords)
         except:
-                print("Something might be wrong!")
-                print("Please check your internet connection and try again.")
-                
+            print("Something might be wrong!")
+            print("Please check your internet connection and try again.")
+
+# If "status" in task it would get the data of the given stock
+    if "status" in task:
+        try:
+            stock = task.replace("status ", "")
+            get_current_stock_data_nse(stock)
+
+        except:
+            print("Something might be wrong!")
+            print("Please check your internet connection and try again.")
+
+    if "-s" in task:
+        try:
+            stock = task.replace("-s ", "")
+            get_current_stock_data_nse(stock)
+
+        except:
+            print("Something might be wrong!")
+            print("Please check your internet connection and try again.")
