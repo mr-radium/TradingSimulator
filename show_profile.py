09de06c3d29bdaed174f0562106f6faf931ae27d
Mr. Radium <ambroseutsav@gmail.com>
2021-01-10 21:04:10 +0530

    initial commit

--- show_profile.py
+++ show_profile.py
@@ -2,10 +2,15 @@ import json
 
 def show_profile():
     profile_data = json.loads(open('profile.json',).read())
+    portfolio_lenght = len(profile_data["having"])
 
     print("")
     print(profile_data["username"] + ',')
     print("Current Balence: " + profile_data["current-balence"])
-    print("---Portfolio---")
     print("")
-    print(profile_data["having"])
+    print("Portfolio")
+    
+    # i = 0
+    # while i > portfolio_lenght:
+    #     print(profile_data["having"][i][""])
+    
