911a2c5443a9a0a7a8938b334db4aaf4270213f3
Mr. Radium <ambroseutsav@gmail.com>
2021-01-11 08:09:22 +0530

    initial commit

--- simulator.py
+++ simulator.py
@@ -6,54 +6,70 @@ from show_profile import show_profile
 
 
 if __name__ == "__main__":
-    task = input("Enter you request: ")
-    
-# If "ls" in task it would do symbol searching
-    if "ls" in task:
+    while True:
+            
+        task = input("Enter you request: ")
+        
+    # If "ls" in task it would do symbol searching
+        if "ls" in task:
 
-        try:
-            keywords = task.replace("ls ", "")
-            sym_search(keywords)
-        except:
-            print("Something might be wrong!")
-            print("Please check your internet connection and try again.")
-
-# If "status" in task it would get the data of the given stock
-    if "status" in task:
-        try:
-            stock = task.replace("status ", "")
-            get_current_stock_data_nse(stock)
-
-        except:
-            print("Something might be wrong!")
-            print("Please check your internet connection and try again.")
-
-    if "-s" in task:
-        try:
-            stock = task.replace("-s ", "")
-            get_current_stock_data_nse(stock)
-
-        except:
-            print("Something might be wrong!")
-            print("Please check your internet connection and try again.")
-
-# If "buy" in task it will buy the stock.
-# The quantity should be assigned followed by "-"
-# Example: "buy ONGC -100"
-    if "buy" in task:
-        try:
-            arranged_task = task.split()
-            stock = arranged_task[1]
             try:
-                quantity = arranged_task[2].replace("-", "")
-                buy_stock(stock, quantity)
+                keywords = task.replace("ls ", "")
+                sym_search(keywords)
+                print()
             except:
-                print("")
-                print("Please assign a quantity at last followed by '-'")
-                print("For example- 'buy ONGC -400' or buy 'buy BURGERKING -25'")
-        except:
-            print("Something might be wrong!")
-            print("Please check your internet connection and try again.")
+                print("Something might be wrong!")
+                print("Please check your internet connection and try again.")
+                print()
 
-    if "-i" in task:
-        show_profile()
+    # If "status" in task it would get the data of the given stock
+        if "status" in task:
+            try:
+                stock = task.replace("status ", "")
+                get_current_stock_data_nse(stock)
+                print()
+
+            except:
+                print("Something might be wrong!")
+                print("Please check your internet connection and try again.")
+                print()
+
+        if "-s" in task:
+            try:
+                stock = task.replace("-s ", "")
+                get_current_stock_data_nse(stock)
+                print()
+
+            except:
+                print("Something might be wrong!")
+                print("Please check your internet connection and try again.")
+                print()
+
+    # If "buy" in task it will buy the stock.
+    # The quantity should be assigned followed by "-"
+    # Example: "buy ONGC -100"
+        if "buy" in task:
+            try:
+                arranged_task = task.split()
+                stock = arranged_task[1]
+                print()
+                try:
+                    quantity = arranged_task[2].replace("-", "")
+                    buy_stock(stock, quantity)
+                    print()
+                except:
+                    print("")
+                    print("Please assign a quantity at last followed by '-'")
+                    print("For example- 'buy ONGC -400' or buy 'buy BURGERKING -25'")
+                    print()
+            except:
+                print("Something might be wrong!")
+                print("Please check your internet connection and try again.")
+                print()
+
+        if "-i" in task:
+            show_profile()
+            print()
+
+        if "-q" in task:
+            exit(code=0)
