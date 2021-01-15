7bcb7df699513b47684d88069c285153b7016d68
Mr. Radium <ambroseutsav@gmail.com>
2021-01-15 18:00:53 +0530

    added some more features

--- show_profile.py
+++ show_profile.py
@@ -29,8 +29,12 @@ def show_profile():
             print(profile_data["portfolio"][i]["bought-company"])
             print("Bought Price: ₹" + profile_data["portfolio"][i]["bought-price"])
             print("Quantity: " + profile_data["portfolio"][i]["bought-quantity"])
-            limited_float_profit = "{:.2f}".format(str(un_comma(stock_info["data"][0]["lastPrice"]) * bought_quantity - total_bought_price))
-            print("\033[31m" + "+" + limited_float_profit)
+
+            # This variable limits the amount of digits after the decimal            
+            limited_float_profit = "{:.2f}".format(un_comma(stock_info["data"][0]["lastPrice"]) * bought_quantity - total_bought_price)
+
+            # This thing prints the profit in green color  
+            print("\033[32m" + "+" + limited_float_profit)
             print("\033[39m")
 
         if un_comma(profile_data["portfolio"][i]["bought-price"]) > un_comma(stock_info["data"][0]["lastPrice"]):
@@ -38,13 +42,23 @@ def show_profile():
             print(profile_data["portfolio"][i]["bought-company"])
             print("Bought Price: ₹" + profile_data["portfolio"][i]["bought-price"])
             print("Quantity: " + profile_data["portfolio"][i]["bought-quantity"])
-            limited_float_loss = "{:.2f}".format(total_bought_price - un_comma(stock_info["data"][0]["lastPrice"]) * bought_quantity)      
+
+            # This variable limits the amount of digits after the decimal
+            limited_float_loss = "{:.2f}".format(total_bought_price - un_comma(stock_info["data"][0]["lastPrice"]) * bought_quantity)    
+
+            # This thing prints the loss in red color  
             print("\033[31m" + "-" + limited_float_loss)
             print("\033[39m")
 
-        else:
-            print("No function for this, haha!")
-            
+        if un_comma(profile_data["portfolio"][i]["bought-price"]) == un_comma(stock_info["data"][0]["lastPrice"]):
+            print("")
+            print(profile_data["portfolio"][i]["bought-company"])
+            print("Bought Price: ₹" + profile_data["portfolio"][i]["bought-price"])
+            print("Quantity: " + profile_data["portfolio"][i]["bought-quantity"])
+
+            # This thing prints the profit in green color  
+            print("\033[32m" + "+00")
+            print("\033[39m")
         i += 1
 
 
