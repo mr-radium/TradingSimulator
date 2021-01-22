783baa2c42546cc6309e417c710bf06c6678bd89
Mr. Radium <ambroseutsav@gmail.com>
2021-01-22 08:42:48 +0530

    Show Profile Completed

--- show_profile.py
+++ show_profile.py
@@ -36,7 +36,7 @@ def show_profile():
             limited_float_profit = "{:.2f}".format(un_comma(stock_info["data"][0]["lastPrice"]) * bought_quantity - total_bought_price)
 
             portfolio_value += float(limited_float_profit)
-            print(portfolio_value)
+            portfolio_profit += float(limited_float_profit)
 
             # This thing prints the profit in green color  
             print("\033[32m" + "+" + limited_float_profit)
@@ -50,6 +50,9 @@ def show_profile():
 
             # This variable limits the amount of digits after the decimal
             limited_float_loss = "{:.2f}".format(total_bought_price - un_comma(stock_info["data"][0]["lastPrice"]) * bought_quantity)    
+
+            portfolio_value -= float(limited_float_loss)
+            portfolio_loss += float(limited_float_loss)
 
             # This thing prints the loss in red color  
             print("\033[31m" + "-" + limited_float_loss)
@@ -65,5 +68,15 @@ def show_profile():
             print("\033[32m" + "+0.00")
             print("\033[39m")
 
-        
+      
         i += 1
+
+    if "-" in str(portfolio_value):
+        portfolio_value = str(portfolio_value).replace("-", "")
+        print("\033[31m" + "Portfolio Value: -₹" + str(portfolio_value))
+    else:
+        print("\033[32m" + "Portfolio Value: +₹" + str(portfolio_value))
+
+    print("\033[32m" + "Portfolio Profit: ₹" + str(portfolio_profit))
+    print("\033[31m" + "Portfolio Loss: ₹" + str(portfolio_loss))
+    print("\033[39m")
