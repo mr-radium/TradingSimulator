18fa8a51353f09905ebcd379b70c852ee24a777e
Mr. Radium <ambroseutsav@gmail.com>
2021-01-24 19:00:41 +0530

    initial commit

--- sell_stock.py
+++ sell_stock.py
@@ -7,4 +7,5 @@ def sell_stock(stock):
     if "&" in stock:
         stock = stock.replace("&", "%26")
     
-    
+    profile_data = json.loads(open('profile.json',).read())
+    portfolio_length = len(profile_data["portfolio"])
